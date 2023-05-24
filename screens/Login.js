8163d6bbaf782cc30417dd1f897996c5af5fca9a
Init project <congpgph26764@fpt.edu.vn>
2023-05-24 12:06:48 +0700

    dangky, dangnhap

--- screens/Login.js
+++ screens/Login.js
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React, {Component} from "react";
 import { Button, StyleSheet, Text, View, Modal, TextInput } from 'react-native';
 import { useState } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const Login = (props) => {
+    const [email, setemail] = useState("");
+    const [password, setpassword] = useState("");
     const [manhinhcho, setmanhinhcho] = useState (false);
 
     const ManHinhCho = () => {
@@ -16,6 +19,57 @@ const Login = (props) => {
           ManHinhCho()
       }, 5000);
       }
+
+      const doLogin =  () => {
+        // kiểm tra hợp lệ dữ liệu
+            if (email.length == 0) {
+                // thông báo:
+                alert("Chưa nhập email")
+                return;
+            }
+            if (password.length == 0) {
+                // thông báo:
+                alert("Chưa nhập password")
+                return;
+            }
+    
+            // url_check_login
+           let url_check_login = 'http://192.168.100.9:3000/api/users?keyword='+email;
+    
+    
+           fetch(url_check_login)
+               .then((res) => {
+                   return res.json();
+               })
+               .then(  async (res_login) => {
+                   if (res_login.length != 1) {
+                       alert("Không tồn tại Email");
+                       return;
+                   } else {
+                       // kiểm tra password
+                       let objU = res_login[0];
+
+                       if (objU.password == password) {
+                           // đúng pass thì lưu vào storage
+    
+    
+                           try {
+                              await AsyncStorage.setItem("login", JSON.stringify(objU) );
+                               props.navigation.navigate('Home');    
+                             } catch (e) {
+                               // saving error
+                               console.log(e);
+                             }
+                          
+                       }else{
+                           alert("Sai password");
+                       }
+                   }
+    
+    
+               })
+    
+        }
 
         React.useEffect (() =>{
             ManHinhCho();
@@ -34,21 +88,21 @@ const Login = (props) => {
             <Text style={{fontSize: 20,width:200, lineHeight: 30, color:"#4E4B66"}}>Welcome back you’ve been missed</Text>
     
             <View style={{display: 'flex', flexDirection: 'row', marginTop: 40, fontSize:14}}>
-                <Text >Username</Text>
+                <Text >Email</Text>
                 <Text style={{color:"red", marginLeft: 5}}>*</Text>
             </View>
-            <TextInput style={{marginTop:10, padding:10, paddingLeft: 10, height:48, borderColor: "#4E4B66", borderWidth: 1, borderRadius: 6}} ></TextInput>
+            <TextInput style={{marginTop:10, padding:10, paddingLeft: 10, height:48, borderColor: "#4E4B66", borderWidth: 1, borderRadius: 6}} onChangeText={(txt)=>{setemail(txt)}}></TextInput>
     
             <View style={{display: 'flex', flexDirection: 'row', marginTop: 16, fontSize:14}}>
                 <Text >Password</Text>
                 <Text style={{color:"red", marginLeft: 5}}>*</Text>
             </View>
-            <TextInput style={{marginTop:10, padding:10, height:48, borderColor: "#4E4B66", borderWidth: 1, borderRadius: 6}}
+            <TextInput style={{marginTop:10, padding:10, height:48, borderColor: "#4E4B66", borderWidth: 1, borderRadius: 6}} onChangeText={(txt)=>{setpassword(txt)}} extContentType="password"
                secureTextEntry={true}></TextInput>
     
-            <Text style={{marginTop:10, textAlign: "right", color: "#1877F2", marginBottom: 23}} >Forgot the password ?</Text>
+            <View style={{height:50}}></View>
     
-            <Button title='Login' onPress={()=>{props.navigation.navigate('Home')}}></Button>
+            <Button title='Login' onPress={doLogin}></Button>
             <View style={{height:20}}/>
 
             <View style={{display: 'flex', flexDirection: 'row', marginTop: 25, fontSize:14, alignSelf:"center"}}>
--- screens/Signup.js
+++ screens/Signup.js
@@ -1,40 +1,114 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {Component, useState} from "react";
-import {Button, StyleSheet, Text, View, TextInput } from 'react-native';
+import {Button, StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
 
 const Signup = (props) => {
 
+    const [fullname, setfullname] = useState("");
+    const [email, setemail] = useState("");
+    const [password, setpassword] = useState("");
+    const [address, setaddress] = useState("");
+    const [phone, setphone] = useState("");
+
+    const doSignup = () =>{
+
+
+        if (fullname.length == 0) {
+
+            alert("Chưa nhập fullname")
+            return;
+        }
+        if (email.length == 0) {
+
+            alert("Chưa nhập email")
+            return;
+        }
+        if (password.length == 0) {
+
+            alert("Chưa nhập password")
+            return;
+        }
+        if (address.length == 0) {
+
+            alert("Chưa nhập address")
+            return;
+        }
+        if (phone.length == 0) {
+
+            alert("Chưa nhập phone number")
+            return;
+        }
+
+
+        let objU = { name: fullname, role: "User", email: email, phone: phone, password: password, address: address };
+
+        let url_users = 'http://192.168.100.9:3000/api/users/add';
+        
+        fetch(url_users, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            
+            body: JSON.stringify(objU)
+        })
+            .then((res) => {
+                if (res.status = 201){
+                    alert("Đăng ký thành công");
+                    props.navigation.navigate('Login')
+                }
+                    
+            })
+            .catch((ex) => {
+                console.log(ex);
+            });
+    }
+
         return (
             <View style={styles.container}>
+                <ScrollView>
                 <Text style={{fontSize: 48, lineHeight: 72, marginTop: 30, color:'#1877F2'}}>Hello!</Text>
                 <Text style={{fontSize: 20,width:200, lineHeight: 30, color:"#4E4B66"}}>Signup to get Started</Text>
         
                 <View style={{display: 'flex', flexDirection: 'row', marginTop: 40, fontSize:14}}>
-                    <Text >Username</Text>
+                    <Text >Full name</Text>
                     <Text style={{color:"red", marginLeft: 5}}>*</Text>
                 </View>
-                <TextInput style={{marginTop:10, padding:10, paddingLeft: 10, height:48, borderColor: "#4E4B66", borderWidth: 1, borderRadius: 6}}></TextInput>
+                <TextInput style={{marginTop:10, padding:10, paddingLeft: 10, height:48, borderColor: "#4E4B66", borderWidth: 1, borderRadius: 6}} onChangeText={(txt)=>{setfullname(txt)}}></TextInput>
         
                 <View style={{display: 'flex', flexDirection: 'row', marginTop: 16, fontSize:14}}>
-                    <Text >Full Name</Text>
+                    <Text >Email</Text>
                     <Text style={{color:"red", marginLeft: 5}}>*</Text>
                 </View>
-                <TextInput style={{marginTop:10, padding:10, paddingLeft: 10, height:48, borderColor: "#4E4B66", borderWidth: 1, borderRadius: 6}}></TextInput>
+                <TextInput style={{marginTop:10, padding:10, paddingLeft: 10, height:48, borderColor: "#4E4B66", borderWidth: 1, borderRadius: 6}} onChangeText={(txt)=>{setemail(txt)}}></TextInput>
                 
                 <View style={{display: 'flex', flexDirection: 'row', marginTop: 16, fontSize:14}}>
                     <Text >Password</Text>
                     <Text style={{color:"red", marginLeft: 5}}>*</Text>
                 </View>
-                <TextInput style={{marginTop:10, padding:10, height:48, borderColor: "#4E4B66", borderWidth: 1, borderRadius: 6, marginBottom: 48}}
-               secureTextEntry={true}></TextInput>
+                <TextInput style={{marginTop:10, padding:10, paddingLeft: 10, height:48, borderColor: "#4E4B66", borderWidth: 1, borderRadius: 6}} onChangeText={(txt)=>{setpassword(txt)}} extContentType="password" secureTextEntry={true}></TextInput>
+
+                <View style={{display: 'flex', flexDirection: 'row', marginTop: 16, fontSize:14}}>
+                    <Text >Address</Text>
+                    <Text style={{color:"red", marginLeft: 5}}>*</Text>
+                </View>
+                <TextInput style={{marginTop:10, padding:10, paddingLeft: 10, height:48, borderColor: "#4E4B66", borderWidth: 1, borderRadius: 6}} onChangeText={(txt)=>{setaddress(txt)}}></TextInput>
+
+                <View style={{display: 'flex', flexDirection: 'row', marginTop: 16, fontSize:14}}>
+                    <Text >Phone Number</Text>
+                    <Text style={{color:"red", marginLeft: 5}}>*</Text>
+                </View>
+                <TextInput style={{marginTop:10, padding:10, height:48, borderColor: "#4E4B66", borderWidth: 1, borderRadius: 6, marginBottom: 48}} onChangeText={(txt)=>{setphone(txt)}}></TextInput>
         
         
-                <Button title='Signup' onPress={()=>{props.navigation.navigate('Login')}}></Button>
+                <Button title='Creat Account' onPress={doSignup}></Button>
 
                 <View style={{display: 'flex', flexDirection: 'row', marginTop: 25, fontSize:14, alignSelf:"center"}}>
                     <Text >Already have an account ? </Text>
-                    <Text style={{color:"#1877F2", marginLeft: 5}} onPress={()=>{props.navigation.navigate('Login')}}>Login</Text>
+                    <Text style={{color:"#1877F2", marginLeft: 5, marginBottom: 40}} onPress={()=>{props.navigation.navigate('Login')}}>Login</Text>
                 </View>
+                </ScrollView>
             </View>
           );
 }
