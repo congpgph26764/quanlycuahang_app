88d6be919895767a0ee57d75ee8fe6ce458d09ff
bactv <bactvph26425@fpt.edu.vn>
2023-06-09 20:34:09 +0700

    chi tiet san pham va trang thai don hang

--- screens/DetailSale.js
+++ screens/DetailSale.js
@@ -7,11 +7,90 @@ import Carousel from 'react-native-snap-carousel';
 
 const Detail = ({route, navigation }) => {
 
+    const { id,name,description,id_category, price, image, image1, image2, quantity, status } = route.params;
+
+    const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+
+    const images = [
+        { id: 1, url: image },
+        { id: 2, url: image1 },
+        { id: 3, url: image2 },
+      ];
+
+   
+
+      const renderItem = ({ item, index }) => {
+        return (
+          <TouchableOpacity onPress={() => setSelectedImageIndex(index)}>
+            <Image
+              source={{ uri: item.url }}
+              style={{ width: 80, height: 80, borderRadius: 5, marginHorizontal: 5 }}
+            />
+          </TouchableOpacity>
+        );
+      };
+
     
     
     return(
         <View style={styles.container}>
-            
+            <View style={styles.herder}>
+
+                    <TouchableOpacity style={{ marginRight: "auto"}} onPress={()=>{navigation.goBack()}}>
+                        <Image style={{width:25, height:25}} source={{uri:"https://cdn-icons-png.flaticon.com/128/3114/3114883.png"}}/>
+                    </TouchableOpacity>
+                    
+                </View>
+                <View style={styles.separator1}/>
+
+
+                <View style={styles.contentContainer}> 
+                    <ScrollView >
+                        
+
+                        <View>
+                            
+                            <Image
+                                source={{ uri: images[selectedImageIndex].url }}
+                                style={{ width: Dimensions.get('window').width, height: 400 }}
+                            />
+                            <Carousel
+                                data={images}
+                                renderItem={renderItem}
+                                sliderWidth={Dimensions.get('window').width}
+                                itemWidth={80}
+                                onSnapToItem={(index) => setSelectedImageIndex(index)}
+                            />
+                        </View>
+
+                        <View style={{marginHorizontal: 20, marginVertical: 20}}>
+                            <Text style={{fontSize:20, marginTop:10}}>{name.toUpperCase()}</Text>
+
+                            <Text style={{ marginTop:10, marginBottom:10}}>{price} đ</Text>
+
+                            <View style={styles.separator1}/>
+
+                            <Text style={{ marginTop:10, marginBottom:10, fontSize:16}}>Product info</Text>
+
+                            <Text style={{color: "#777777"}}>{description}</Text>
+                        </View>
+                        
+                    </ScrollView>
+                    
+                </View>
+                    
+                            
+
+                <View style={styles.separator1}/>
+                <View style={styles.footer}>
+                    <View style={{display: 'flex', flexDirection: 'row',}}>
+                        <TouchableOpacity style={{marginRight:15}}>
+                            <Text style={{paddingVertical: 20, paddingHorizontal: 30, borderColor:"#000", borderWidth: 1}}>Size: Oversize</Text>
+                        </TouchableOpacity>
+                        
+                    </View>     
+                    
+                </View>
         </View>
     );
 }
--- screens/OderStatusSale.js
+++ screens/OderStatusSale.js
@@ -8,7 +8,80 @@ const OderStatusSale = (props) => {
 
     return (
         <View style={styles.container}>
-            
+            <View style={styles.herder}>
+                <Text style={{fontSize:20}}>Oder Status</Text>  
+            </View>
+                <View style={styles.separator1}/>
+
+
+                <View style={styles.contentContainer}> 
+                    <ScrollView >
+
+                        <View style={{marginHorizontal: 20}}>
+
+
+                            <TouchableOpacity style={{ flexDirection: 'row', marginTop:40 }} onPress={()=>{props.navigation.navigate('OrderSale',{status: 'Chờ xác nhận'})}}>
+                                <Image style={{width:25, height:25}} source={{uri:"https://cdn-icons-png.flaticon.com/128/2169/2169864.png"}}/>
+                                <View style={{marginLeft:40}}>
+                                    <Text style={{fontSize: 20}}>Chờ xác nhận</Text>
+                                </View>
+                                <Image style={{width:20, height:20, marginLeft: "auto"}} source={{uri:"https://cdn-icons-png.flaticon.com/128/2889/2889731.png"}}/>
+                            </TouchableOpacity >
+
+                            <View style={styles.separator}/>
+
+                            <TouchableOpacity style={{ flexDirection: 'row' }} onPress={()=>{props.navigation.navigate('OrderSale',{status: 'Chờ lấy hàng'})}}>
+                                <Image style={{width:25, height:25}} source={{uri:"https://cdn-icons-png.flaticon.com/128/2630/2630085.png"}}/>
+                                <View style={{marginLeft:40}}>
+                                    <Text style={{fontSize: 20}}>Chờ lấy hàng</Text>
+                                </View>
+                                <Image style={{width:20, height:20, marginLeft: "auto"}} source={{uri:"https://cdn-icons-png.flaticon.com/128/2889/2889731.png"}}/>
+
+                            </TouchableOpacity >
+
+                            <View style={styles.separator}/>
+
+                            <TouchableOpacity style={{ flexDirection: 'row' }} onPress={()=>{props.navigation.navigate('OrderSale',{status: 'Đang giao'})}}>
+                                <Image style={{width:25, height:25}} source={{uri:"https://cdn-icons-png.flaticon.com/128/709/709790.png"}}/>
+                                <View style={{marginLeft:40}}>
+                                    <Text style={{fontSize: 20}}>Đang giao</Text>
+                                </View>
+                                <Image style={{width:20, height:20, marginLeft: "auto"}} source={{uri:"https://cdn-icons-png.flaticon.com/128/2889/2889731.png"}}/>
+
+                            </TouchableOpacity >
+
+                            <View style={styles.separator}/>
+
+                            <TouchableOpacity style={{ flexDirection: 'row' }} onPress={()=>{props.navigation.navigate('OrderSale',{status: 'Đã hoàn thành'})}}>
+                                <Image style={{width:25, height:25}} source={{uri:"https://cdn-icons-png.flaticon.com/128/2961/2961948.png"}}/>
+                                <View style={{marginLeft:40}}>
+                                    <Text style={{fontSize: 20}}>Đã hoàn thành</Text>
+                                </View>
+                                <Image style={{width:20, height:20, marginLeft: "auto"}} source={{uri:"https://cdn-icons-png.flaticon.com/128/2889/2889731.png"}}/>
+
+                            </TouchableOpacity >
+
+                        </View>
+
+                    </ScrollView>
+
+                </View>
+                    
+                            
+
+                <View style={styles.separator1}/>
+                <View style={styles.footer}>
+                    <TouchableOpacity onPress={()=>{props.navigation.navigate('HomeSale')}}>
+                        <Image style={{width:25, height:25}} source={{uri:"https://cdn-icons-png.flaticon.com/512/1946/1946488.png"}}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{marginLeft: 80}} onPress={()=>{props.navigation.navigate('OderStatusSale')}}>
+                        <Image style={{width:27, height:27}} source={{uri:"https://cdn-icons-png.flaticon.com/128/665/665916.png"}}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{marginLeft: 80}} onPress={()=>{props.navigation.navigate('ProfileSale')}}>
+                        <Image style={{width:25, height:25}} source={{uri:"https://cdn-icons-png.flaticon.com/512/1144/1144760.png"}}/>
+                    </TouchableOpacity>       
+                    
+                </View>
         </View>
     );
 }
